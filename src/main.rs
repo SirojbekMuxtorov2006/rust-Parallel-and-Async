b0c259bf64e11f23d2660d9fde071cfeac49a5c9
SirojbekMuxtorov2006 <sirojbekmuxtorov9@gmail.com>
2026-06-09 12:26:44 +0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::SQRT_2;
+
 fn is_prime(n: u64) -> bool {
     if n < 2 {
         return false;
@@ -12,6 +14,14 @@ fn is_prime(n: u64) -> bool {
     }
 
     let sqrt_n = (n as f64).sqrt() as u64;
+
+    for i in (3..=sqrt_n).step_by(2) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    true
 }
 
 fn main() {
